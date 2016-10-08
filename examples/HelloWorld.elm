c0fdf4a04eabc8c33a6dd699d46cb2f3fc490927
Michael Twomey <mick@twomeylee.name>
2016-10-08 17:22:41 +0100

    wip

--- examples/HelloWorld.elm
+++ examples/HelloWorld.elm
@@ -12,15 +12,23 @@ update : Model -> Model
 update model =
     { model | t = model.t + 1 }
 
+draw_letter t i j0 =
+    let
+        j = 7 - j0
+        col = 7 + j
+        t1 = t + i*4 - j*2
+        -- x = cos(t0) * 5
+        x = 5 -- Bug in PICO-8 example, cos(nil) * 5 -> 1 * 5
+        y = 38 + j + cos(t1/50) * 5
+    in
+        [ Console.palette(7, col)
+        , Console.sprite(16+i, 8+i*8 + x, y)
+        ]
+
 draw : Console.Console -> List Console.Command
 draw console =
-    [ Console.putPixel 0 0 Palette.peach
-    , Console.putPixel 10 10 Palette.peach
-    , Console.putPixel 50 50 Palette.peach
-    , Console.putPixel 100 100 Palette.peach
-    , Console.putPixel 127 127 Palette.peach
-    , Console.print "Hello" 20 20 Palette.orange
-    ]
+    List.concat 
+        List.map2 (\i j -> draw_letter model.t i j) [1..11] [0..7] 
 
 main : Program Never
 main =
