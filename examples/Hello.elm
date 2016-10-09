1ffda31bf4cc855f2e4ce8ce933d7ae100ee6487
Michael Twomey <mick@twomeylee.name>
2016-10-09 16:54:03 +0100

    WIP

--- examples/Hello.elm
+++ examples/Hello.elm
@@ -1,18 +1,28 @@
 import Elmo8.Console as Console
 import Elmo8.Palettes.Pico8 as Palette
 
-draw : Console.Console -> List Console.Command
-draw console =
-    [ Console.putPixel 0 0 Palette.peach
-    , Console.putPixel 10 10 Palette.peach
-    , Console.putPixel 50 50 Palette.peach
-    , Console.putPixel 100 100 Palette.peach
-    , Console.putPixel 127 127 Palette.peach
-    , Console.print "Hello" 20 20 Palette.orange
-    ]
+type alias Model = { drawn : Bool }
+
+draw : Console.Console Model -> Model ->  List Console.Command
+draw console model =
+    case model.drawn of 
+        True -> []
+        False -> 
+            [ Console.putPixel 0 0 Palette.peach
+            , Console.putPixel 10 10 Palette.peach
+            , Console.putPixel 50 50 Palette.peach
+            , Console.putPixel 100 100 Palette.peach
+            , Console.putPixel 127 127 Palette.peach
+            , Console.print "Hello" 20 20 Palette.orange
+            ]
+
+update : Model -> Model
+update model = { drawn = True }
 
 main : Program Never
 main =
  Console.boot
     { draw = draw
+    , init = { drawn = False }
+    , update = update
     }
--- examples/HelloWorld.elm
+++ examples/HelloWorld.elm
@@ -1,5 +1,8 @@
+{-| PICO-8's Hello.lua redone in ELMO-8
+
+-}
+
 import Elmo8.Console as Console
-import Elmo8.Palettes.Pico8 as Palette
 
 type alias Model = {
     t : Int
@@ -12,26 +15,30 @@ update : Model -> Model
 update model =
     { model | t = model.t + 1 }
 
+draw_letter : Int -> Int -> Int -> List Console.Command
 draw_letter t i j0 =
     let
         j = 7 - j0
         col = 7 + j
         t1 = t + i*4 - j*2
         -- x = cos(t0) * 5
-        x = 5 -- Bug in PICO-8 example, cos(nil) * 5 -> 1 * 5
+        -- Bug in PICO-8 example, cos(nil) * 5 -> 1 * 5
+        x = 5
         y = 38 + j + cos(t1/50) * 5
     in
-        [ Console.palette(7, col)
-        , Console.sprite(16+i, 8+i*8 + x, y)
+        [ Console.palette 7 col
+        , Console.sprite (16+i) (8+i*8 + x)  (round y)
         ]
 
-draw : Console.Console -> List Console.Command
-draw console =
-    List.concat 
-        List.map2 (\i j -> draw_letter model.t i j) [1..11] [0..7] 
+draw : Console.Console Model -> Model ->  List Console.Command
+draw console model =
+    List.map2 (\i j -> draw_letter model.t i j) [1..11] [0..7]
+        |> List.concat
 
 main : Program Never
 main =
  Console.boot
     { draw = draw
+    , init = init
+    , update = update
     }
