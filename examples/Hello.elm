307a277ebfe76d759910bf271cfc9f8eb8c4e693
Michael Twomey <mick@twomeylee.name>
2016-10-19 23:32:43 +0000

    Update Hello.elm

--- examples/Hello.elm
+++ examples/Hello.elm
@@ -1,28 +1,23 @@
 import Elmo8.Console as Console
 import Elmo8.Palettes.Pico8 as Palette
 
-type alias Model = { drawn : Bool }
+type alias Model = {}
 
 draw : Console.Console Model -> Model ->  List Console.Command
 draw console model =
-    case model.drawn of 
-        True -> []
-        False -> 
-            [ Console.putPixel 0 0 Palette.peach
-            , Console.putPixel 10 10 Palette.peach
-            , Console.putPixel 50 50 Palette.peach
-            , Console.putPixel 100 100 Palette.peach
-            , Console.putPixel 127 127 Palette.peach
-            , Console.print "Hello" 20 20 Palette.orange
-            ]
+    [ Console.sprite 2 60 30
+    , Console.putPixel 0 0 Palette.peach
+    , Console.putPixel 127 127 Palette.peach
+    ]
 
 update : Model -> Model
-update model = { drawn = True }
+update model = model
 
 main : Program Never
 main =
  Console.boot
     { draw = draw
-    , init = { drawn = False }
+    , init = { }
     , update = update
+    , spritesUri = "/birdwatching.png"
     }
